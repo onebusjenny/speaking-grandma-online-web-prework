fe7b11d1d6e6c8acdc0ebc5ceb81dbae7e69bd48
Jenny Chu <jchudesign0930@gmail.com>
2019-02-03 04:47:43 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,18 @@
+
+
+if me == "normal voice".upcase
+  return "huh?! speak up, sonny!".upcase
+elsif me == "shout".upcase
+  return "no, not since 1938!".upcase
+elsif me == "I love you grandma".upcase
+  return "I lovce you too pumpkin!".upcase
+else 
+  puts "I will just ignore you"
+
+
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
